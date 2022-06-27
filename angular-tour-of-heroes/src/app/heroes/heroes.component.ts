09eba4cf61beff78948f41b3cd2f02883f45f288
Souadalimess <99185694+Souadalimess@users.noreply.github.com>
2022-06-27 19:07:21 +0200

    feat: Update HeroesComponent

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,18 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
-  
 
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {}
 
