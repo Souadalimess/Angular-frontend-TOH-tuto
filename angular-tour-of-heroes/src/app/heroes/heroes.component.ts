d7ce461fec55201279778c65f83f7ed1636099f8
Souadalimess <99185694+Souadalimess@users.noreply.github.com>
2022-06-28 12:59:47 +0200

    feat: Add a new hero (POST)

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -11,6 +11,7 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
@@ -20,5 +21,14 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 }
