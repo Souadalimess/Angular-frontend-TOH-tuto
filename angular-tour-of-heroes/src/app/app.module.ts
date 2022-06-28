6f0753ed4572d308d9c97132746c87e2d3eeba41
Souadalimess <99185694+Souadalimess@users.noreply.github.com>
2022-06-28 13:13:32 +0200

    feat: search for heroesmodule

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -11,9 +11,10 @@ import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent, HeroSearchComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule,
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
